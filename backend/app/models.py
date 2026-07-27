0740493e05a15ff717169c4710750b524d00011f
LuizEduardo-service <luizeduardo.service@outlook.com.br>
2026-07-27 16:40:19 -0300

    Add texto2 field to Frase model

--- backend/app/models.py
+++ backend/app/models.py
@@ -10,4 +10,5 @@ class Frase(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     texto = Column(String, nullable=False)
+    texto2 = Column(String, nullable=False)
     criado_em = Column(DateTime, default=datetime.utcnow, nullable=False)
