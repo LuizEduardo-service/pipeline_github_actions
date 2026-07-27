2e67f57dd604a1dab23f0c78df346aaa2857b37c
LuizEduardo-service <luizeduardo.service@outlook.com.br>
2026-07-27 16:45:22 -0300

    Remove texto2 field from Frase model

--- backend/app/models.py
+++ backend/app/models.py
@@ -10,5 +10,4 @@ class Frase(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     texto = Column(String, nullable=False)
-    texto2 = Column(String, nullable=False)
     criado_em = Column(DateTime, default=datetime.utcnow, nullable=False)
